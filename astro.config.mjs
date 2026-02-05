2f00e993332379588162c6504fdf6d72885f93ea
iaalvearp <iaalvearp@gmail.com>
2026-02-04 19:35:33 -0500

    Arreglando falla de subida en cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://woodtong.pages.dev',
     output: 'server',
     adapter: cloudflare({
         imageService: 'cloudflare',
